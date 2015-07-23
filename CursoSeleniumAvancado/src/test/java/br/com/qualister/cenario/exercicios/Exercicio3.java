68b38abe341b3cf1e45aae58ee60518c0a61b372
ricardoramos <ricardoramos@icmc.usp.br>
2015-07-23 01:29:46 -0300

    Formatando

--- CursoSeleniumAvancado/src/test/java/br/com/qualister/cenario/exercicios/Exercicio3.java
+++ CursoSeleniumAvancado/src/test/java/br/com/qualister/cenario/exercicios/Exercicio3.java
@@ -4,21 +4,24 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
+import br.com.qualister.core.WDSAntiga;
+
+
 public class Exercicio3 {
 	@Test
 	public void test(){
-		WDS.get().navigate().to("http://qualister.info/treinamentos/selenium/webdriver/exercicios/avancado/");
-		WDS.get().findElement(By.linkText("Auto Completar")).click();
-		WDS.get().switchTo().frame("paginas");	
-		WDS.get().findElement(By.id("estado_autocomplete")).sendKeys("Rio");
+		WDSAntiga.get().navigate().to("http://qualister.info/treinamentos/selenium/webdriver/exercicios/avancado/");
+		WDSAntiga.get().findElement(By.linkText("Auto Completar")).click();
+		WDSAntiga.get().switchTo().frame("paginas");	
+		WDSAntiga.get().findElement(By.id("estado_autocomplete")).sendKeys("Rio");
 		
 		//WebDriverWait wait = new WebDriverWait(WDS.get(), 10);
-		WDS.getWait().until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//li[contains(text(),'Grande do Sul')]")));
+		WDSAntiga.getWait().until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//li[contains(text(),'Grande do Sul')]")));
 		
-		WDS.get().findElement(By.xpath("//li[contains(text(),'Grande do Sul')]")).click();
-		WDS.get().findElement(By.id("cidade_autocomplete")).sendKeys("Porto");
-		WDS.get().findElement(By.xpath("//li[contains(text(),'Alegre')]")).click();
-		WDS.get().findElement(By.id("proximo")).click();
+		WDSAntiga.get().findElement(By.xpath("//li[contains(text(),'Grande do Sul')]")).click();
+		WDSAntiga.get().findElement(By.id("cidade_autocomplete")).sendKeys("Porto");
+		WDSAntiga.get().findElement(By.xpath("//li[contains(text(),'Alegre')]")).click();
+		WDSAntiga.get().findElement(By.id("proximo")).click();
 		
 		//WDS.get().findElement(By.xpath("//li[contains(test(), 'Grande do Sul')]")).sendKeys("Rio");
 		
